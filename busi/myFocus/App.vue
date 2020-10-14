62254e829ef50a0b215ac3e2f98104bcef9d3651
zhangyanlong <yanlongzhang1990@gmail.com>
2020-10-14 10:35:41 +0800

    我的关注列表

---
+++ busi/myFocus/App.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="container-myFocus">
+    <div class="bg-page"></div>
+    <div class="list-focus" v-if="list && list.length">
+      <div class="item-focus" v-for="(item, index) in list" :key="index">
+        <div class="info-focus">
+          <div class="user-info">
+            <div class="name">张三</div>
+            <div class="base">（1091891 研发组）</div>
+          </div>
+          <div class="time-focus">关注时间：2020-10-14</div>
+        </div>
+        <div class="btn-focus">已关注</div>
+      </div>
+    </div>
+    <div class="empty" v-if="!list || !list.length">
+      <img class="icon-empty" src="./assets/icon-empty.png" alt="">
+      暂无关注
+    </div>
+    <div class="btn-bottom">
+      <div class="btn">
+        <img class="icon-add" src="./assets/icon-add.png" alt="">
+        搜索关注
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container-myFocus {
+  padding-bottom: 158px;
+  .bg-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    z-index: -1;
+    background-color: #F6F6F6;
+  }
+  .list-focus {
+    width: 100%;
+    padding: 0 24px;
+    background: #fff;
+    .item-focus {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      height: 140px;
+      padding: 0 8px;
+      border-bottom: 1px solid #EDEDED;
+      &:last-child {
+        border: 0;
+      }
+      .info-focus {
+        .user-info {
+          display: flex;
+          align-items: flex-end;
+          .name {
+            font-size: 30px;
+            color: #333333;
+            line-height: 42px;
+            font-weight: bolder;
+          }
+          .base {
+            font-size: 24px;
+            color: #333333;
+            line-height: 34px;
+          }
+        }
+        .time-focus {
+          margin-top: 14px;
+          font-size: 24px;
+          color: #999;
+          line-height: 34px;
+        }
+      }
+      .btn-focus {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100px;
+        height: 46px;
+        font-size: 24px;
+        color: #B7B7B7;
+        border: 1px solid #DADAD9;
+        &.act {
+          border: 1px solid #FFC844;
+          background: #FFC844;
+          color: #fff;
+        }
+      }
+    }
+  }
+  .empty {
+    margin: 132px auto 0;
+    width: 270px;
+    text-align: center;
+    font-size: 28px;
+    color: #BDC7D3;
+    line-height: 40px;
+    .icon-empty {
+      margin-bottom: 40px;
+      width: 270px;
+      height: 270px;
+    }
+  }
+  .btn-bottom {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    padding: 20px 24px 48px;
+    background: #F6F6F6;
+    z-index: 1;
+    .btn {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 702px;
+      height: 90px;
+      background: #FF592F;
+      border-radius: 10px;
+      font-size: 30px;
+      color: #fff;
+      line-height: 42px;
+      font-weight: bolder;
+      .icon-add {
+        margin-right: 12px;
+        width: 26px;
+        height: 26px;
+      }
+    }
+  }
+}
+</style>
